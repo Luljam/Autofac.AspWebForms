abc7de43abdf2e8c60b3f91e1a7cf1d5b0665669
AVANADE-CORP\luciano.f.de.brito <luciano.f.de.brito@avanade.com>
2021-11-24 22:45:28 -0300

    Install Autofac packages and implement IContainerProviderAccessor

--- WebFormClient/Global.asax.cs
+++ WebFormClient/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac.Integration.Web;
+using System;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
@@ -6,8 +7,14 @@ using System.Web.Routing;
 
 namespace WebFormClient
 {
-    public class Global : HttpApplication
+    public class Global : HttpApplication, IContainerProviderAccessor
     {
+        static IContainerProvider _ContainerProvider;
+
+        public IContainerProvider ContainerProvider
+        {
+            get { return _ContainerProvider; }
+        }
         void Application_Start(object sender, EventArgs e)
         {
             RouteConfig.RegisterRoutes(RouteTable.Routes);
